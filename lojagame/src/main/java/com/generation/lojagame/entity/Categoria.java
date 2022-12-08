2df31fc59dd7127959fec423bea25c08c6802c61
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2022-12-08 00:05:02 -0300

    relacionando a entidade categoria e produto On To Many

--- lojagame/src/main/java/com/generation/lojagame/entity/Categoria.java
+++ lojagame/src/main/java/com/generation/lojagame/entity/Categoria.java
@@ -1,17 +1,38 @@
 package com.generation.lojagame.entity;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
-@Data
-@Entity
-@Table(name="tb_categoria")
+
+@Table(name = "tb_categoria")
 public class Categoria {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String categoria;
+
+	@OneToMany
+	@JsonIgnoreProperties("categoria")
+	private Produto produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
 }
